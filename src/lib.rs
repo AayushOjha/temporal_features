2214ab2d76919bfc554ff826f011bdf2f9ee154f
Ayush Ojha <ayush@hunch.in>
2025-12-02 15:01:03 +0530

    feat: Add PyO3 integration to expose Rust functions to Python and implement automated PyPI releases via GitHub Actions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
+use pyo3::prelude::*;
+
+/// Adds two numbers together.
+#[pyfunction]
+fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// A Python module implemented in Rust.
+#[pymodule]
+fn temporal_features(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add, m)?)?;
+    Ok(())
 }
